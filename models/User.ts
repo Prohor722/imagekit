9ba3cdffc54ed87db98e85fe28effac04792586c
Prohor722 <prohor.b722@gmail.com>
2025-07-11 00:00:05 +0600

    add

--- models/User.ts
+++ models/User.ts
@@ -19,3 +19,12 @@ const userSchema = new Schema<IUser>(
         timestamps: true,
     }
 );
+
+userSchema.pre('save', async function (next) 
+    {
+        if(this.isModified('password')) {
+            this.password = await bcrypt.hash(this.password, 10);
+        }
+        next();
+    }
+);
