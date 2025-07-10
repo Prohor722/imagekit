81876c720e1185058075b8e5ea8165382beceb57
Prohor722 <prohor.b722@gmail.com>
2025-07-10 23:54:37 +0600

    user and videos model added

--- models/User.ts
+++ models/User.ts
@@ -5,4 +5,17 @@ import bcrypt from "bcryptjs";
 export interface IUser {
   email: string;
   password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
+
+const userSchema = new Schema<IUser>(
+    {
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+    },
+    {
+        timestamps: true,
+    }
+);
