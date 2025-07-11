aaffa6c12f37cbe32f7fcfef7ccb2d3f1b3af68c
Prohor722 <prohor.b722@gmail.com>
2025-07-11 23:55:31 +0600

    video model added

--- models/Video.ts
+++ models/Video.ts
@@ -1,5 +1,4 @@
 import mongoose, {Schema, model, models} from "mongoose";
-import bcrypt from "bcryptjs";
 
 export const VIDEO_DIMENSIONS = {
     width: 1080,
@@ -22,3 +21,24 @@ export interface IVideo {
     transformations?: Transformations
 }
 
+const videoSchema = new Schema<IVideo>(
+     {
+        title: {type: String, required: true},
+        description: {type: String, required: true},
+        videoUrl: {type: String, required: true},
+        thumbnailUrl: {type: String, required: true},
+        controls: {type: Boolean, default: true},
+        transformations: {
+            height: {type: Number, default: VIDEO_DIMENSIONS.height},
+            width: {type: Number, default: VIDEO_DIMENSIONS.width},
+            quality: {type: Number, min:1, max:100},
+        }
+     },
+     {
+        timestamps: true,
+     }
+);
+
+const Video = models?.Video || model<IVideo>('Video', videoSchema);
+
+export default Video;
