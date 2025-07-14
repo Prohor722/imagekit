db4d4575432dac33668750abac9baac4bd65f7b8
Prohor722 <prohor.b722@gmail.com>
2025-07-14 21:20:08 +0600

    credential auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,50 @@
+import { bcrypt } from 'bcryptjs';
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import { dbConnect } from "@/lib/db";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/models/User";
 
 export const authOptions:NextAuthOptions = {
     providers: [    
-        GithubProvider({      
-            clientId: process.env.GITHUB_ID!,      
-            clientSecret: process.env.GITHUB_SECRET!,    
-        }),    // ...add more providers here  ],
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {label: "email", type: "text"},
+                password: {label: "password", type: "password"},
+            },
+            async authorize(credentials){
+                if(credentials?.email || credentials?.password){
+                    throw new Error("Email and password are required");
+                }
+
+                try{
+                    await dbConnect();
+                    const user = await User.findOne({ email: credentials?.email })
+
+                    if(!user){
+                        throw new Error("No user found with the given email");
+                    }
+
+                    const isPassMatch = await bcrypt.compare(
+                        credentials?.password,
+                        user.password
+                    )
+
+                    if(!isPassMatch){
+                        throw new Error("Incorrect password");
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        email: user.email,
+                    }
+                }
+                catch(error){
+                    console.error("Error in authorize:", error);
+                    throw new Error("Internal Server Error");
+                }
+            }
+        })
     ],
 };
 
