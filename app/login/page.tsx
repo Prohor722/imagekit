f4ebf8e60d525a12b2862b44a5f77a7a58abb1d0
Prohor722 <prohor.b722@gmail.com>
2025-07-17 22:23:13 +0600

    login page functionality done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import { Validators } from '../methods/validator';
+import { signIn } from 'next-auth/react';
 
 const LoginPage = () => {
     const {
@@ -10,10 +11,33 @@ const LoginPage = () => {
         handleChange,
       } = Validators();
     const router = useRouter();
+    
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const result = await signIn("credentials",{
+            email,
+            password,
+            redirect: false,
+        })
+
+        if(result?.error) {
+            alert("Invalid credentials");
+        } else {
+            router.push("/");
+        }
+        
+    }
 
 
   return (
-    <div>Login</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="email" placeholder='Email' value={email} onChange={(e) => handleChange(e, "email")} />
+            <input type="password" placeholder='Password' value={password} onChange={(e) => handleChange(e, "password")} />
+            <button type='submit'>Login</button>
+        </form>
+    </div>
   )
 }
 
