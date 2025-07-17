ca4956af082691eba295dc95698939423614d207
Prohor722 <prohor.b722@gmail.com>
2025-07-18 00:09:11 +0600

    middleware added

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,31 @@
-import {withAuth} from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-    function middleware(req) {
-        return NextResponse.next()
-)
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        const { pathname } = req.nextUrl;
+        if (
+          pathname === "/" ||
+          pathname.startsWith("/api/videos") ||
+          pathname.startsWith("/login") ||
+          pathname.startsWith("/register") ||
+          pathname.startsWith("/api/videos")
+        ) {
+          return true;
+        }
+
+        return !!token;
+      },
+    },
+  }
+);
 
 export const config = {
-    // Match all routes except for the ones starting with _next/static, _next/image, favicon.ico, and public
-    matcher: [
-        "/((?!_next/static|_next/image|favicon.ico|public).*)",
-    ]
-}
+  // Match all routes except for the ones starting with _next/static, _next/image, favicon.ico, and public
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|public).*)"],
+};
